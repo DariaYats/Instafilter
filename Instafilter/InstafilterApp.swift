24e3122f388d8dcdfa102afae31811c4c9c43a33
Daria Yats <dariayatsyniuk@gmail.com>
2025-05-28 15:53:23 +0200

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Дарья Яцынюк on 28.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
