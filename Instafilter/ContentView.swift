ee19bec37acadccb88ce72757df32cea81180220
Daria Yats <dariayatsyniuk@gmail.com>
2025-05-29 16:56:14 +0200

    fix bug

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -45,12 +45,14 @@ struct ContentView: View {
                 .padding(.vertical)
                 
                 HStack{
-                    Button("Change filter") {
-                        changeFilter()
+                    Button("Change filter", action: changeFilter)
+
+                    Spacer()
+
+                    if let processedImage {
+                        ShareLink(item: processedImage, preview: SharePreview("Instafilter image", image: processedImage))
                     }
                 }
-                
-                Spacer()
             }
         }
         .padding([.horizontal, .bottom])
@@ -77,19 +79,17 @@ struct ContentView: View {
             guard let inputImage = UIImage(data: imageData) else { return }
 
             let beginImage = CIImage(image: inputImage)
-
-            let inputKeys = currentFilter.inputKeys
-
-            if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)}
-            if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey)}
-            if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey)}
-
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
             applayProcessing()
         }
     }
 
     func applayProcessing() {
-        currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
+        let inputKeys = currentFilter.inputKeys
+
+        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)}
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey)}
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey)}
 
         guard let outputImage = currentFilter.outputImage else { return }
         guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
