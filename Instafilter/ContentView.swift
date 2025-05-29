1fdd9252e02be316b4644e38e4a1f64ec895e591
Daria Yats <dariayatsyniuk@gmail.com>
2025-05-29 15:47:27 +0200

    initial commit

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,17 +5,80 @@
 //  Created by Дарья Яцынюк on 28.05.2025.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    @State private var selectedItem: PhotosPickerItem?
+
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Spacer()
+
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                    }
+                }
+                .onChange(of: selectedItem, loadImage)
+
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity, applayProcessing)
+                }
+                .padding(.vertical)
+                
+                HStack{
+                    Button("Change filter") {
+                        changeFilter()
+                    }
+                }
+                
+                Spacer()
+            }
         }
-        .padding()
+        .padding([.horizontal, .bottom])
+        .navigationTitle("Instafilter")
+    }
+    
+    func changeFilter() {
+
+    }
+
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return }
+
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            applayProcessing()
+        }
+    }
+
+    func applayProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+
+        let uiImage = UIImage(cgImage: cgImage)
+        processedImage = Image(uiImage: uiImage)
     }
 }
 
